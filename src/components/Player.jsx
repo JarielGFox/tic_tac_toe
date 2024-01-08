462c329d227d2c416d173ea507ff679b384db7f9
Giovanni Crudele <crudelegio@gmail.com>
2024-01-08 17:14:40 +0100

    best practice on rendering state and use of handleChange

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,39 @@
 import { useState } from "react";
 
-function Player({ name, symbol }) {
+function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const handleChange = (event) => {
+    //event è l'oggetto che vogliamo emettere
+    //accediamo alla proprietà target.value, dato che event è un oggetto
+    //in questo modo accediamo al valore che l'utente sta digitando in input in quel momento
+    setPlayerName(event.target.value);
+  };
 
   const handleEditClick = () => {
-    //aggiorniamo lo stato che ha valore di default false
-    setIsEditing(true);
+    //serve per cambiare lo stato in maniera dinamica ed in tempo reale
+    setIsEditing((editing) => !editing);
   };
 
   // dichiariamo nella variabile playerName lo span dove introdurre il nome del giocatore
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   //valore condizionale di isEditing, ossia se isEditing è true, mostramo l'input per inserire il nome
   if (isEditing) {
-    playerName = <input type="text" required />;
+    editablePlayerName = (
+      <input type="text" required onChange={handleChange} value={playerName} />
+    );
   }
 
   return (
     <li>
       <span className="player">
         {/* richiamiamo playerName in modo dinamico, dove in base al valore dello State, mostrerà il nome o l'input */}
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
         <button className="button" onClick={handleEditClick}>
-          Edit
+          {isEditing ? "Save" : "Edit"}
         </button>
       </span>
     </li>
